2f61a836b91c8b2aa5b16cf9ff061d489590bc37
0x0sp1d3r <42107203+0x0sp1d3r@users.noreply.github.com>
2022-10-25 19:24:35 +0200

    took a break from bst, added ideas in comments and get_hash() is done.

--- HashSet.py
+++ HashSet.py
@@ -12,16 +12,30 @@ class HashSet:
         self.buckets = [[] for i in range(8)]
 
     # Computes hash value for a word (a string)
-    def get_hash(self, word):
-        pass    # Placeholder code ==> to be replaced
+    def get_hash(self, word): # this should be done 
+        hashh = 0
+        for l in word:
+            hashh += ord(l)  # get the ascii value and just add it 
+        # maybe retrun the hash % the len(buckets) so i dont repeat my self ??
+        return hashh  # return the ascii value of the whole word AKA hash
+
 
     # Doubles size of bucket list
     def rehash(self):
+        for i in range(len(self.buckets)):
+            if len(i) > 2 and [for j in range() ]:
+                pass # brb 
+
         pass    # Placeholder code ==> to be replaced
 
     # Adds a word to set if not already added
     def add(self, word):
-        pass    # Placeholder code ==> to be replaced
+        # use the len of the buckets [how many buckets we have] as a mod number.
+        mod = len(self.buckets)
+        pos = self.get_hash(word) % mod
+        self.buckets[pos] # append using the pos 
+        # i need to think about the rehashing case here....
+        # increase the size every time we add a bucket ?
 
     # Returns a string representation of the set content
     def to_string(self):
@@ -33,6 +47,9 @@ class HashSet:
 
     # Returns True if word in set, otherwise False
     def contains(self, word):
+        # im repeating code, maybe put it in the hashing func ?
+        mod = len(self.buckets)
+        pos = self.get_hash(word)
         pass    # Placeholder code ==> to be replaced
 
     # Returns current size of bucket list
